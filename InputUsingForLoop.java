7643fa0dc329ff6b45af00530c745b548406558a
Arnav Bhagwat <76836227+Thindino@users.noreply.github.com>
2023-05-03 22:28:26 +0530

    Update InputUsingForLoop.java

--- InputUsingForLoop.java
+++ InputUsingForLoop.java
@@ -13,6 +13,13 @@ public class arrayUsingForLoop {
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+        // Another easy way to do it is this 
+        
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = input.nextInt();
+
+        }
+        System.out.println(Arrays.toString(arr)); // line basically converts array to a string and prints it.
 
 
 
