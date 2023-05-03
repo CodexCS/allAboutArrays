dc1b9183869d91de57655fa650701bd9dd26e8af
Arnav Bhagwat <76836227+Thindino@users.noreply.github.com>
2023-05-03 22:19:09 +0530

    Add files via upload

---
+++ arrayUsingForLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class arrayUsingForLoop {
+
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] arr =  new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+
+
+    }
+}
